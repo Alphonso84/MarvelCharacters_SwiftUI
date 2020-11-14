08519a1e0f062c9abd4212e53571ab8a4a976528
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2020-11-14 08:08:20 -0800

    Initial Commit

---
+++ MarvelCharacters_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarvelCharacters_SwiftUI
+//
+//  Created by Alphonso Sensley II on 11/14/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarvelCharacters_SwiftUI/MarvelCharacters_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelCharacters_SwiftUIApp.swift
+//  MarvelCharacters_SwiftUI
+//
+//  Created by Alphonso Sensley II on 11/14/20.
+//
+
+import SwiftUI
+
+@main
+struct MarvelCharacters_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
